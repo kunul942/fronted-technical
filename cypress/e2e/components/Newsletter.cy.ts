84fdff4aaea3beaf58c1b0f8ff7ed8e5d632f701
kleedev942 <klee@kaleidoscopic.dev>
2024-07-09 19:04:15 -0600

    feat: cypress testing and github actions

--- cypress/e2e/components/Newsletter.cy.ts
+++ cypress/e2e/components/Newsletter.cy.ts
@@ -1,9 +1,6 @@
-describe('template spec', () => {
-    it('passes', () => {
-        cy.visit('http://localhost:3000/');
-
-        cy.get('[data-testid=cypress-newsletter-title]')
-            .should('exist')
-            .should('have.text', 'Stay in touch');
+describe('Newsletter component', () => {
+    it('displays the newsletter signup message in local', () => {
+        cy.visit('/');
+        cy.contains('Stay in touch');
     });
 });
